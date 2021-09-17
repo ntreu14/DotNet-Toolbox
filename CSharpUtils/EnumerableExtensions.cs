63c33c236b8ac64963666bf94722a01d15b28687
Nicholas Treu <treunicholas@gmail.com>
2021-09-17 15:14:09 -0600

    Implement Scan for IEnumerable.:

--- CSharpUtils/EnumerableExtensions.cs
+++ CSharpUtils/EnumerableExtensions.cs
@@ -6,12 +6,28 @@ namespace CSharpUtils
 {
     public static class EnumerableExtensions
     {
-        public static (IEnumerable<T> whenTrue, IEnumerable<T> whenFalse) Partition<T>(this IEnumerable<T> xs, Func<T, bool> predicate)
+        public static (IEnumerable<T> whenTrue, IEnumerable<T> whenFalse) Partition<T>(this IEnumerable<T> source, Func<T, bool> predicate)
         {
-            var splitByPasses = xs.ToLookup(predicate);
+            var splitByPasses = source.ToLookup(predicate);
             return (splitByPasses[true], splitByPasses[false]);
         }
 
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T> xs) => xs == null || !xs.Any();
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> source) => 
+            source == null || !source.Any();
+
+        public static IEnumerable<TState> Scan<T, TState>(this IEnumerable<T> source, TState state, Func<TState, T, TState> folder)
+        {
+            var currentState = state;
+            var accumulatingStates = new List<TState> { state };
+
+            foreach (var element in source)
+            {
+                var nextState = folder(currentState, element);
+                accumulatingStates.Add(nextState);
+                currentState = nextState;
+            }
+
+            return accumulatingStates;
+        }
     }
 }
